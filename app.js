af330cd93f6a33381faa639163ce3f2948b324c8
Sean Dez <sd@seandezoysa.com>
2019-01-17 20:54:41 +0700

    connect passport local strat to sequelize/postgres

--- app.js
+++ app.js
@@ -4,11 +4,19 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const passport      = require("passport"),
+      LocalStrategy = require("passport-local"),
+      session       = require("express-session"),
+      models        = require("./models");
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+app.use(session({secret : "acute zumba plant"}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -22,6 +30,32 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+// passport.use('local', new LocalStrategy({
+//   usernameField : 'username',
+//   passwordField : 'password',
+//   passReqToCallback : true
+// }, (req, username, password, done) => {
+//   console.log("inside localStrategy callback");
+//   process.nextTick(() => {
+//     models.UserAccount.find({
+//       where : {
+//         username : username
+//       }
+//     }).then((userRecord, error) => {
+//       if (error) { return done(error); }
+//       else if (userRecord === false) { return done(null, false); }
+//       else if (password !== userRecord.password) { return done(null, false); }
+//       else if (password === bcrypt.compare(userRecord.password)) {
+//         return done(null, userRecord); // goes into req.user
+//       }
+//     })
+//   })
+// }));
+//
+// passport.serializeUser((user, done) => { done(null, user) });
+// passport.deserializeUser((user, done) => { done(null, user) });
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,40 +1,49 @@
 var express = require('express');
 var router = express.Router();
-
-const dbConfig = {
-  operatorsAliases: false
-};
-
-const Sequelize = require("sequelize");
-const sequelize = new Sequelize(process.env.DB_URI, dbConfig);
-// const sequelize = new Sequelize(process.env.DB_URI);
-sequelize.authenticate()
-         .then(() => {
-           console.log("sequelize connected to postgres");
-         })
-         .catch(error => console.log('error: ', error));
+const models   = require("../models"),
+      passport = require("passport"),
+      LocalStrategy = require("passport-local"),
+      bcryptjs = require("bcryptjs");
 
 
-const TestModel = sequelize.define('testModel', {
-  firstName : {
-    type : Sequelize.STRING
-  },
-  lastName : {
-    type : Sequelize.STRING
-  }
-});
 
-TestModel.sync().then(() => {
-  // create Table
-  return TestModel.create({
-    firstName : '2nd entry',
-    lastName : 'lastN2ndEntry'
-  });
-});
 
-TestModel.findAll().then(users => {
-  console.log('displaying users: ', users)
-});
+passport.use('local', new LocalStrategy({
+  usernameField : 'username',
+  passwordField : 'password',
+  passReqToCallback : true
+}, (req, username, password, done) => {
+  
+  process.nextTick(() => {
+    models.UserAccount.find({
+      where : {
+        username : username
+      }
+    }).then((userRecord, error) => {
+      if (error) {
+        console.log('=================== 1 error =====================');
+      return done(error);
+      }
+      else if (userRecord === false) {
+        console.log('============== 2 no userRecord found ================');
+        return done(null, false);
+      }
+      bcryptjs.compare(req.body.password, userRecord.password, (error, compareResult) => {
+        if (compareResult === false) {
+          console.log('================= 3 pw mismatch ====================');
+          return done(null, false);
+        } else if (compareResult) {
+          console.log('=============== 4 successful local strat ==============');
+          return done(null, userRecord); // goes into req.user
+        }
+      })
+    })
+  })
+}));
+
+passport.serializeUser((user, done) => { done(null, user) });
+passport.deserializeUser((user, done) => { done(null, user) });
+
 
 
 
@@ -44,4 +53,59 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+
+
+// find the user.
+  // If none, create and then authenticate
+router.post('/sign-up', (req, res, next) => {
+  models.UserAccount.findOne({ where : { username : req.body.username }})
+  .then(userRecord => {
+    console.log("inside first .then()");
+    const hashedPassword = bcryptjs.hashSync(req.body.password, 10);
+    console.log(userRecord);
+    if (!userRecord) {
+      console.log("inside if !userRecord");
+      models.UserAccount.create({
+        username : req.body.username,
+        password : hashedPassword
+      })
+      .then(userRecord => {
+      console.log("----------- User Created ------------");
+      console.log(userRecord);
+      passport.authenticate('local', (error, user, info) => {
+        if (error) { return next(error); }
+        if (userRecord == false) {
+          console.log("------------ USER RECORD AGAIN ----------");
+          console.log(userRecord);
+          return res.json({ "error" : "User data is false" });
+        } // should never trigger
+  
+        console.log("----------- user object in pp.authenticate ---------");
+        console.log(user); // false means auth failed
+        req.logIn(userRecord, error => {
+          if (error) return next(error);
+          return res.json({ "userInfo" : userRecord });
+        })
+        })(req, res, next)
+      })
+    } else if (userRecord) {
+        console.log ("user already exists: routes/index.js:39");
+        return res.json({ "error" : "User already exists" })
+    } else {
+      console.log("Hit the else clause"); res.json({"error" : "Hit else clause"})
+    }
+  })
+});
+
+router.post('/log-in', passport.authenticate('local'), (req, res) => {
+  return res.json({ "loggedInUser" : {
+      "id" : req.user.id,
+      "username" : req.user.username
+    }
+  })
+});
+
+
+
 module.exports = router;
+
